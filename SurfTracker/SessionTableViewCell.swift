a1d404c956a28acf572877f5becabe89e41b1c4f
Cian Brassil <mrb@live.ie>
2016-09-25 15:42:29 +0100

    Initial session table view cell work

---
+++ SurfTracker/SessionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SessionTableViewCell.swift
+//  SurfTracker
+//
+//  Created by Cian Brassil on 25/09/2016.
+//  Copyright © 2016 Cian Brassil. All rights reserved.
+//
+
+import UIKit
+
+class SessionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
