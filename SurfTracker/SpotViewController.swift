6f545b43e4d302ea40be132e7853f2afa14d20d0
Cian Brassil <cian@Cians-MacBook-Pro.local>
2017-04-11 21:08:38 +1000

    Add/Edit spot page added
    Included ‘+’ in spot list. Current crashes app when you press that
    though…

---
+++ SurfTracker/SpotViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SpotViewController.swift
+//  SurfTracker
+//
+//  Created by Cian Brassil on 11/4/17.
+//  Copyright © 2017 Cian Brassil. All rights reserved.
+//
+
+import UIKit
+
+class SpotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
