0504447e033880f0bfe7ccc972927190950f5bd0
Cian Brassil <mrb@live.ie>
2017-04-29 16:01:09 +1000

    Added basic spot page UI elements

--- SurfTracker/SpotViewController.swift
+++ SurfTracker/SpotViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class SpotViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    // MARK: Navigation
+    
+    // MARK: Actions
 
     override func viewDidLoad() {
         super.viewDidLoad()
