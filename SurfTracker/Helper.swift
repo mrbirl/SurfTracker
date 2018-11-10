7ce985f1170c37fedd8398651bfd1452cd84a76b
mrbirl <mrb@live.ie>
2018-11-10 17:24:29 +1100

    Mid length date
    Looks a little better maybe

--- SurfTracker/Helper.swift
+++ SurfTracker/Helper.swift
@@ -122,7 +122,7 @@ class Helper{
     // Take session time (Date) and return formatted string
     static func timeToString(sessionTime: Date) -> String{
         let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.dateStyle = DateFormatter.Style.medium
         dateFormatter.timeStyle = .short
         return dateFormatter.string(from: sessionTime)
     }
